9b2dd52b20d7fee6bafd17055e78e001e7c7775b
Daniel Fernando Lourusso <dflourusso@gmail.com>
2015-11-03 10:24:58 -0200

    add pt-BR language

--- src/inflector.js
+++ src/inflector.js
@@ -73,6 +73,55 @@ angular.module('platanus.inflector', [])
           [new RegExp('(quiz)zes$', 'gi'),                                                   '$1'],
           [new RegExp('s$', 'gi'),                                                           '']
         ]
+      },
+      'pt-BR': {
+        uncountable: ['tórax', 'tênis', 'ônibus', 'lápis', 'fênix' ],
+
+        plural: [
+          [new RegExp('^(japon|escoc|ingl|dinamarqu|fregu|portugu)(es|ês)', 'gi'), '$1eses'],
+          [new RegExp('([^aeou])il$', 'gi'),                                       '$1is'],
+          [new RegExp('^(paí)s$', 'gi'),                                           '$1ses'],
+          [new RegExp('(z|r)$', 'gi'),                                             '$1es'],
+          [new RegExp('al$', 'gi'),                                                'ais'],
+          [new RegExp('el$', 'gi'),                                                'eis'],
+          [new RegExp('ol$', 'gi'),                                                'ois'],
+          [new RegExp('ul$', 'gi'),                                                'uis'],
+          [new RegExp('m$', 'gi'),                                                 'ns'],
+          [new RegExp('^(|g)ás$', 'gi'),                                           '$1ases'],
+          [new RegExp('^(alem|c|p)ão$', 'gi'),                                     '$1ães'],
+          [new RegExp('(irm|m)ão$', 'gi'),                                         '$1ãos'],
+          [new RegExp('ão$', 'gi'),                                                '$1ões'],
+
+          [new RegExp('^(alem|c|p)ao$', 'gi'),                                     '$1aes'],
+          [new RegExp('^(irm|m)ao$', 'gi'),                                        '$1aos'],
+          [new RegExp('ao$', 'gi'),                                                'oes'],
+
+          [new RegExp('s$', 'gi'),                                                 's'],
+          [new RegExp('$', 'gi'),                                                  's']
+        ],
+
+        singular: [
+          [new RegExp('^(á|gá|paí)ses$', 'gi'),                                    '$1s'],
+          [new RegExp('(r|z)es$', 'gi'),                                           '$1'],
+          [new RegExp('([^p])ais$', 'gi'),                                         '$1al'],
+          [new RegExp('eis$','gi'),                                                'el'],
+          [new RegExp('ois$','gi'),                                                'ol'],
+          [new RegExp('uis$','gi'),                                                'ul'],
+          [new RegExp('(r|t|f|v)is$','gi'),                                        '$1il'],
+          [new RegExp('ns$', 'gi'),                                                'm'],
+          [new RegExp('sses$', 'gi'),                                              'sse'],
+          [new RegExp('^(.*[^s]s)es$', 'gi'),                                      '$1'],
+          [new RegExp('ães$', 'gi'),                                               'ão'],
+          [new RegExp('aes$', 'gi'),                                               'ao'],
+          [new RegExp('ãos$', 'gi'),                                               'ão'],
+          [new RegExp('aos$', 'gi'),                                               'ao'],
+          [new RegExp('ões$', 'gi'),                                               'ao'],
+          [new RegExp('oes$', 'gi'),                                               'ao'],
+          [new RegExp('(japon|escoc|ingl|dinamarqu|fregu|portugu)eses$', 'gi'),    '$1es'],
+          [new RegExp('^(g|)ases$', 'gi'),                                         '$1as'],
+          [new RegExp('([^ê])s$', 'gi'),                                           '$1'],
+          [new RegExp('s$', 'gi'),                                                 '']
+        ]
       }
     };
 
